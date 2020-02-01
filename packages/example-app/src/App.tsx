b5c4b2c4341430d5e55993fc05e418ec16837854
Luis <takahashihideki408@gmail.com>
2020-02-01 15:01:36 -0300

    Added example app + ts-library fixes

---
+++ packages/example-app/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import BLA from '@rollup-playground/ts-library';
+
+console.log({ BLA })
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Mudei { BLA(12) }
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
