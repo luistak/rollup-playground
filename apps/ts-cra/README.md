0be01e161cac4d597dcc5f7e704c16293023777a
Luis <takahashihideki408@gmail.com>
2020-04-24 00:08:05 -0300

    Refactored apps and packages

---
+++ apps/ts-cra/README.md
@@ -0,0 +1,17 @@
+# Create React App
+
+Libby:
+
+```Javascript
+// @ts-ignore
+import Libby from '@rollup-playground/libby';
+
+console.log({ ['10 + 5']: Libby(10) })
+```
+
+/:
+
+```Javascript
+// @ts-ignore
+import Simple from '@rollup-playground/simple';
+```
--- apps/ts-cra/src/App.tsx
+++ apps/ts-cra/src/App.tsx
@@ -3,24 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 import BLA from '@rollup-playground/ts-library';
-import { Bla } from '@rollup-playground/ts-react';
-
-// @ts-ignore
-import aaa from '@rollup-playground/simple';
-// @ts-ignore
-import libby from '@rollup-playground/libby';
-
-console.log({ ['10 + 5']: libby(10) })
+import { Bla as MyComponentLib } from '@rollup-playground/ts-react';
 
 const App = () => {
+  const five = 5;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Mudei { BLA(12) }
+          Five { BLA(five) }
         </p>
-        <Bla name="Testing" age={23} />
+        <MyComponentLib name="Testing" age={23} />
         <a
           className="App-link"
           href="https://reactjs.org"
